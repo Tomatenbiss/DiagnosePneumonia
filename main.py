6e18fb4819ef1ed4523c34e505e6cc5b1595dcfb
Tom Bisson <tom.bisson@student.htw-berlin.de>
2018-07-09 17:29:25 +0200

    Changed architecture to only one dropout layer. Increased dropout rate to 0.5.

--- main.py
+++ main.py
@@ -16,26 +16,42 @@ from keras.callbacks import ReduceLROnPlateau
 image_height = 150
 image_width = 150
 batch_size = 32
-no_of_epochs  = 2
+no_of_epochs  = 300
+
+# model = Sequential()
+# model.add(Conv2D(64,(3,3),input_shape=(image_height,image_width,3),activation='relu'))
+# model.add(Conv2D(64,(3,3),activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.2))
+# model.add(Conv2D(32,(3,3),activation='relu'))
+# model.add(Conv2D(32,(3,3),activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.2))
+# model.add(Conv2D(16,(3,3),activation='relu'))
+# model.add(Conv2D(16,(3,3),activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.2))
+# model.add(Flatten())
+# model.add(Dense(units=128,activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(units=1,activation='sigmoid'))
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) #precision/recall AUC, ROC, R2-Score, F-Score
 
 model = Sequential()
 model.add(Conv2D(64,(3,3),input_shape=(image_height,image_width,3),activation='relu'))
 model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.2))
+model.add(Dropout(0.5))
 model.add(Conv2D(32,(3,3),activation='relu'))
 model.add(Conv2D(32,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.2))
 model.add(Conv2D(16,(3,3),activation='relu'))
 model.add(Conv2D(16,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(units=128,activation='relu'))
-model.add(Dropout(0.2))
 model.add(Dense(units=1,activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) #precision/recall AUC, ROC, R2-Score, F-Score
 
 model.summary()
 
